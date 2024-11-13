e4a54c128fe5d63b034b35910804f93e0ee5a52d
Jon Johansson <jonjohansson88@gmail.com>
2024-11-13 09:32:33 +0100

    Add project files.

---
+++ Snake_v1.0_Facit/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Snake_v1._0_Facit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new App();
+            app.GoGame();
+        }
+    }
+}
